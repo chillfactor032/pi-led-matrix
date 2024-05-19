b8167cae746b03914290219acb649483e509cb02
chillfactor032 <68230787+chillfactor032@users.noreply.github.com>
2024-05-19 00:30:09 -0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,5 +1,10 @@
+import os
 import board
 import neopixel
+import requests
+from io import BytesIO
+from PIL import Image, GifImagePlugin
+GifImagePlugin.LOADING_STRATEGY = GifImagePlugin.LoadingStrategy.RGB_ALWAYS
 
 class LedMatrix():
 
@@ -16,6 +21,46 @@ class LedMatrix():
             auto_write=False, 
             pixel_order=self.ORDER)
         self.index_map = LedMatrix.gen_index_map(self.height, self.width)
+    
+    def show_img(self, url):
+        img_pixels = self.fetch_img_pixels(url)
+        if img_pixels is None:
+            return
+        for x in range(img_pixels):
+            self.pixels[x] = img_pixels[x]
+            
+    def fetch_img_pixels(self, img_url):
+        img = None
+        if img_url[0:4] == "http":
+            # Naive way to tell if this is a url vs a local file
+            resp = requests.get(img_url, headers=self.user_agent)
+            resp.raise_for_status()
+            img = Image.open(BytesIO(resp.content))
+        else:
+            if not os.path.isfile(img_url):
+                raise FileNotFoundError("Image File Does Not Exist")
+            img = Image.open(img_url)
+        img.thumbnail((self.width, self.height))
+        thumb_w, thumb_h = img.size
+        if thumb_h != self.height or thumb_w != self.width:
+            return None
+        pixels = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if y >= thumb_h or x >= thumb_w:
+                    color = (0, 0, 0)
+                    continue
+                else:
+                    color = img.getpixel((x, y))
+                if type(color) is int:
+                    color = (color, color, color)
+                else:
+                    if len(color) >= 4 and color[3] == 0:
+                        # If transparent, set color to black
+                        color = (0, 0, 0)
+                    color = (color[0], color[1], color[2])
+                pixels.append(color)
+        return pixels
     
     def clear(self):
         self.pixels.fill((0,0,0))
